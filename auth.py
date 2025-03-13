78f4b10bf751df7bb53a623da1d6fffc268344b1
oinqq <insolencem1.1@gmail.com>
2025-03-13 22:08:54 +0200

    Registration finished , need to test

--- auth.py
+++ auth.py
@@ -1,2 +1,12 @@
 import bcrypt
 
+def hash_password(password):
+    salt = bcrypt.gensalt()
+    hashed = bcrypt.hashpw(password=password.encode(),salt=salt)
+    return hashed
+
+def check_password(password , hashed):
+    return bcrypt.checkpw(password.encode(),hashed)
+
+
+
--- routes/register.py
+++ routes/register.py
@@ -1,16 +1,20 @@
-from flask import request , flash
+from flask import request, flash, render_template
 
 from start_app import app
 
+from models.users import add_to_table
 
+from auth import hash_password
 
-
-
-
+from flask import redirect
 
 
 @app.get('/registration/')
-def registration():
+def registration_get():
+    return render_template('registration.html')
+
+@app.post('/registration/')
+def registration_post():
     name = request.form['name']
     email = request.form['email']
     password = request.form['password']
@@ -18,3 +22,6 @@ def registration():
     if not name or not email or not password:
         flash("Name or email or password are not exist")
 
+    add_to_table(name,email, hash_password(password))
+
+    redirect("log_in.html")

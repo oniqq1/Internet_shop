53602f56113e157b2ea916b0b6ce4ffd58b33349
oinqq <insolencem1.1@gmail.com>
2025-03-19 18:54:18 +0200

    user page

--- app/routes/user_page.py
+++ app/routes/user_page.py
@@ -1,6 +1,4 @@
-from flask_login import login_required
-from app import session
-from app import app
+from app import app ,session
 
 from app.models.users import check_user_by_name
 
@@ -8,7 +6,8 @@ from app.models.users import check_user_by_name
 
 @app.get('/user/<name>/')
 def user_page_get(name:str):
-    if check_user_by_name(name):
-        return "all good"
-    else:
-        return "No such account"
+    if session.get('name') in check_user_by_name(session.get('name')):
+        if check_user_by_name(name):
+            return "all good"
+        else:
+            return "No such account"

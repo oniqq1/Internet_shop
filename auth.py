41372316095e35c0015b887aa30b754d61c0abf6
oinqq <insolencem1.1@gmail.com>
2025-03-21 11:59:11 +0200

    delete something to pull request

--- auth.py
+++
@@ -1,9 +0,0 @@
-import bcrypt
-
-def hash_password(password):
-    salt = bcrypt.gensalt()
-    hashed = bcrypt.hashpw(password=password.encode(),salt=salt)
-    return hashed
-
-def check_password(password , hashed):
-    return bcrypt.checkpw(password.encode(),hashed)
--- routes/start_app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, render_template
-
-app = Flask(__name__,template_folder='../templates',static_folder='../static')
-
-
-
-@app.get("/")
-def test():
-    return render_template('index.html')

b506db4080afacb7a0af09f8bb80f4f1d99dc19b
oinqq <insolencem1.1@gmail.com>
2025-03-27 18:55:40 +0200

    роллбак 4

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,7 @@ from secret import key
 from connection import create_table
 
 
-app = Flask(__name__,template_folder='../templatess',static_folder='../static')
+app = Flask(__name__,template_folder='../templates',static_folder='../static')
 app.secret_key = key
 session = session
 
